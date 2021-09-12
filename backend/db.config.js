c83e4f27b6340ecfa00082b113771459e19bf43f
CISSE Fatoumata <fatou.ycisse@gmail.com>
2021-09-12 17:15:10 +0200

    db.config.js

---
+++ backend/db.config.js
@@ -0,0 +1,28 @@
+const mysql = require('mysql');
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize({
+
+    host: '127.0.0.1',
+
+    user: 'root',
+
+    password: 'maman94',
+
+    database: 'groupomania_development',
+
+    dialect: "mysql"
+
+});
+
+const db = {};
+try {
+    db;
+    console.log('Connecté à la base de données MySQL!');
+} catch (error) {
+    console.error('Impossible de se connecter à MySQL :', error);
+}
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;

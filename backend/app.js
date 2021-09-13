48e7fe49e78b2a99d1ae778d889569c833d5b8f7
CISSE Fatoumata <fatou.ycisse@gmail.com>
2021-09-13 21:30:43 +0200

    app.js

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,8 @@ const cors = require('cors'); // Plugin d'appels d'API
 const bodyParser = require('body-parser');
 require("./db.config");
 
+app.disable("x-powered-by");
+
 // middleware general 1 pour definir les headers de toutes les requetes
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -29,11 +31,6 @@ app.use(bodyParser.json());
 // middleware qui permet l'accès statique à des images _dirname= nom du dossier
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use(cors());
-app.use('/api/post', postsRoutes);
-app.use('/api/users', usersRoutes);
-app.use('/api/post', commentsRoutes);
-
 // sanitize des entrées contre les attaques XXS
 app.use(xss());
 
@@ -42,6 +39,14 @@ app.use(helmet());
 
 // Limiter plusieurs sessions dans un court laps de temps pour éviter les attaques de force
 app.use(rateLimit());
+
+
+app.use(cors());
+
+app.use('/api/post', postsRoutes);
+app.use('/api/users', usersRoutes);
+app.use('/api/post', commentsRoutes);
+
 
 // Exportation de l'app pour utilisation par d'autres fichiers, nottament le serveur Node.
 module.exports = app;
